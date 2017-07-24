61d8387ea656f75beab0c2fba6e71fd722206166
C.H.A.D.o <chado@chado.by>
2017-07-24 12:42:56 +0300

    Always send active state

--- deal.go
+++ deal.go
@@ -33,7 +33,7 @@ type Deal struct {
 	FloorPrice  float64      `json:"floor_price,omitempty"`
 	Code        string       `json:"code"`
 	Name        string       `json:"name"`
-	Active      bool         `json:"active,omitempty"`
+	Active      bool         `json:"active"`
 	StartDate   string       `json:"start_date,omitempty"`
 	EndDate     string       `json:"end_date,omitempty"`
 	Type        *Type        `json:"type,omitempty"`
